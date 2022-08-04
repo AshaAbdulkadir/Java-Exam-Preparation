b891e4ec71371a0b399d4363065d2f92a770aede
AshaAbdu <halge87@yahoo.com>
2022-08-04 16:51:09 -0500

    2nd commit

--- src/Methods/methods.java
+++ src/Methods/methods.java
@@ -1,13 +1,18 @@
 package Methods;
 
 public class methods {
-	static void myMethod() {
-		System.out.println("I just got it!");
+	static void myMethod(String firstName) {
+		//System.out.println("I just got it!");
+		System.out.println(firstName + " Abdulkadir");
 	}
 
 	public static void main(String[] args) {
-		myMethod();
+		//myMethod();
+		myMethod("Asha");// add a string argument as the data type the method above takes 
+		myMethod("Halima");
+	
 		
 	}
+
 
 }
