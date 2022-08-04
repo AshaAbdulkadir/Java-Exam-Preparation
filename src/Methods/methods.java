1e5d167c465324524383b997546402583f1ae9c7
AshaAbdu <halge87@yahoo.com>
2022-08-04 16:14:48 -0500

    my 1st commit

---
+++ src/Methods/methods.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class methods {
+	static void myMethod() {
+		System.out.println("I just got it!");
+	}
+
+	public static void main(String[] args) {
+		myMethod();
+		
+	}
+
+}
